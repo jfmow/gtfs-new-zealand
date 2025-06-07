bea30f21908456adee558b918f892a4bb8d17cfa
james mowat <james1mowat@gmail.com>
2025-06-07 14:28:03 +1200

    move up

--- frontend/components/nav.tsx
+++ frontend/components/nav.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
         <>
             {isMobile ? (
                 <nav className='fixed bottom-0 left-0 right-0 z-[10]'>
-                    <div className='w-fit mb-2 mx-auto bg-background/80 backdrop-blur-sm shadow-sm border px-4 py-2 rounded-[9999px]'>
+                    <div className='w-fit mb-4 mx-auto bg-background/80 backdrop-blur-sm shadow-sm border px-4 py-2 rounded-[9999px]'>
                         <ul className='w-full grid grid-cols-5 gap-4 justify-items-center'>
                             {NAV_ROUTES.map((item) => (
                                 <li key={item.label}>
